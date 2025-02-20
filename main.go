8abef2b74ae64f0d869477a106a13013e4b66f66
peter wang <wangbo.red@gmail.com>
2025-02-20 22:21:34 +0800

    release v0.1.6

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ ENVIRONMENT:
 	`, cli.AppHelpTemplate)
 	app := &cli.App{
 		Usage:   "A eBPF-based trace tool like ftrace funcgraph",
-		Version: "0.1.5",
+		Version: "0.1.6",
 		Commands: []*cli.Command{
 			{
 				Name:  "debug",
